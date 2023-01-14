7f8196a26c16e7b373af4e91f34687e7974e7874
ozgun balta <ozgunbalta@Icloud.com>
2023-01-14 12:08:13 +0300

    Refactoring

---
+++ MvcWebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreMVCDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryManager;
+
+        public CategoryController(ICategoryService categoryManager)
+        {
+            _categoryManager = categoryManager;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _categoryManager.GetAll();
+            return View(result.Data);
+        }
+    }
+}
